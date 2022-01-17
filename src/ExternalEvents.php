5f77d35cf6ef4e2fbed617a9ed03d29de2095ccd
Alex Rubia <alexander.rubia@softonic.com>
2022-01-17 16:34:45 +0100

    Fix arguments closure from decorate listener

--- src/ExternalEvents.php
+++ src/ExternalEvents.php
@@ -52,13 +52,13 @@ class ExternalEvents
 
     public static function decorateListener(string $class): \Closure
     {
-        return static function (array $message) use ($class) {
+        return static function (string $event, array $message) use ($class) {
             try {
                 $eventParameter = new ReflectionParameter([$class, 'handle'], 0);
                 $className      = $eventParameter->getType()->getName();
 
-                $event = ExternalEvents::decode($className, $message['data']);
-                (new $class())->handle($event);
+                $event = ExternalEvents::decode($className, $message[0]['data']);
+                resolve($class)->handle($event);
             } catch (ReflectionException $e) {
                 throw new BadMethodCallException(
                     "$class must have a handle method with a single parameter of type object child of \Google\Protobuf\Internal\Message"
